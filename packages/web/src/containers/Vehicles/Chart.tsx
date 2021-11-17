a546b2f28c2eea6afcdd9d325025e8c17d282d9f
Marcin Makarewicz <marcin@makarewicz.eu>
2021-11-17 22:06:44 +0100

    feat(web): add tooltip

--- packages/web/src/containers/Vehicles/Chart.tsx
+++ packages/web/src/containers/Vehicles/Chart.tsx
@@ -1,21 +1,8 @@
-import React, {
-  useCallback,
-  useEffect,
-  useLayoutEffect,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
-import {
-  axisBottom,
-  axisLeft,
-  interpolate,
-  line,
-  select,
-  easeLinear,
-  scaleLinear,
-} from "d3";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { axisBottom, axisLeft, select, scaleLinear } from "d3";
 import cx from "classnames";
+import { Subject, of } from "rxjs";
+import { delay, switchMap } from "rxjs/operators";
 import styles from "./Chart.module.scss";
 
 const { width, height, left, right, top, bottom } = {
@@ -48,6 +35,11 @@ const PROPS = {
 // https://betterprogramming.pub/construct-d3-charts-in-react-cfecc2848ae2
 // https://wattenberger.com/blog/react-and-d3
 export default function Chart({ list }) {
+  const [selected, setSelected] = useState<{
+    top: number;
+    left: number;
+    label: string;
+  } | null>(null);
   const [{ xProp, yProp }, setProps] = useState(() => ({
     xProp: "transactionalPrice",
     yProp: "newPrice",
@@ -56,10 +48,26 @@ export default function Chart({ list }) {
   const yAxisRef = useRef(null);
   const containerRef = useRef(null);
 
+  // https://stackoverflow.com/questions/62066799/custom-tooltip-with-rxjs
+  const selected$ = useMemo(() => new Subject(), []);
+  useEffect(() => {
+    const subscription = selected$
+      .pipe(
+        switchMap((selected) => of(selected).pipe(delay(selected ? 0 : 800)))
+      )
+      .subscribe((selected) => setSelected(selected));
+
+    return () => subscription.unsubscribe();
+  }, [selected$]);
+
   useEffect(() => {
     const dataset = list
       .slice(0, 2000)
-      .map(({ item }) => [item[xProp], item[yProp]]);
+      .map(({ item }) => [
+        item[xProp],
+        item[yProp],
+        `[${item.id}] ${item.title}`,
+      ]);
 
     const xx = dataset.map(([x = 0]) => x);
     const yy = dataset.map(([, y = 0]) => y);
@@ -89,6 +97,10 @@ export default function Chart({ list }) {
             .attr("cy", ([, y = 0]) => yScale(y))
             .attr("r", 0)
             .attr("fill", ([x = 0]) => color(x))
+            .on("mouseover", (e, [, , label]) =>
+              selected$.next({ left: e.layerX, top: e.layerY, label })
+            )
+            .on("mouseout", (e, i) => selected$.next(null))
             .call((enter) =>
               enter.transition().duration(200).attr("r", 2).style("opacity", 1)
             ),
@@ -156,6 +168,14 @@ export default function Chart({ list }) {
         <g transform={`translate(0, ${height - bottom})`} ref={xAxisRef} />
         <g transform={`translate(${left}, 0)`} ref={yAxisRef} />
       </svg>
+      {selected && (
+        <div
+          className={styles.Tooltip}
+          style={{ top: selected.top, left: selected.left }}
+        >
+          {selected.label}
+        </div>
+      )}
     </div>
   );
 }
