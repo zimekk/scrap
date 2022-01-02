a17bb2d6de324d63332da672555ff3b807fde8fb
Marcin Makarewicz <marcin@makarewicz.eu>
2022-01-02 14:53:25 +0100

    feat(web): add games category filter

--- packages/web/src/containers/Games/index.tsx
+++ packages/web/src/containers/Games/index.tsx
@@ -72,13 +72,39 @@ const unify = ({
 function Data({ version = "v1" }) {
   const { results } = asset.read(version); // As many cache keys as you need
 
-  const [search, setSearch] = useState("");
-  const [filter] = useDebounce(search);
-
-  const onChangeSearch = useCallback(
-    ({ target }) => setSearch(target.value),
-    []
+  const options = useMemo(
+    () => ({
+      category: Object.keys(
+        results.reduce(
+          (
+            result: object,
+            {
+              Properties: { Categories },
+            }: { Properties: { Categories: [string] } }
+          ) =>
+            Object.assign(
+              result,
+              (Categories || []).reduce(
+                (result, Category) =>
+                  Object.assign(result, { [Category]: Category }),
+                {}
+              )
+            ),
+          { "": "" }
+        )
+      ).sort((a, b) => a.localeCompare(b)),
+    }),
+    [results]
   );
+
+  const [filters, setFilters] = useState(() => ({
+    category: "",
+    search: "",
+    priceFrom: PRICE_LIST[0],
+    priceTo: PRICE_LIST[PRICE_LIST.length - 1],
+  }));
+
+  const [filter] = useDebounce(filters.search);
 
   const [sortBy, setSortBy] = useState(() => Object.keys(SORT_BY)[0]);
 
@@ -87,37 +113,40 @@ function Data({ version = "v1" }) {
     []
   );
 
-  const [priceFrom, setPriceFrom] = useState(PRICE_LIST[0]);
-  const [priceTo, setPriceTo] = useState(PRICE_LIST[PRICE_LIST.length - 1]);
-
   const onChangePriceFrom = useCallback(
     ({ target }) =>
-      setPriceTo((to) => {
-        const from = Number(target.value);
-        setPriceFrom(from);
-        return to < from ? from : to;
+      setFilters(({ priceTo, ...criteria }) => {
+        const priceFrom = Number(target.value);
+        return {
+          ...criteria,
+          priceFrom,
+          priceTo: priceTo < priceFrom ? priceFrom : priceTo,
+        };
       }),
     []
   );
 
   const onChangePriceTo = useCallback(
     ({ target }) =>
-      setPriceFrom((from) => {
-        const to = Number(target.value);
-        setPriceTo(to);
-        return to > from ? from : to;
+      setFilters(({ priceFrom, ...criteria }) => {
+        const priceTo = Number(target.value);
+        return {
+          ...criteria,
+          priceFrom: priceTo > priceFrom ? priceFrom : priceTo,
+          priceTo,
+        };
       }),
     []
   );
 
-  console.log({ results });
+  console.log({ options, filters, results });
 
   const list = useMemo(
     () =>
       results
         .map(({ _history = {}, ...item }: any) => ({
           _history: [item]
-            .concat(Object.values(_history))
+            .concat(Object.values(_history).reverse())
             .map(unify)
             .filter(
               (item, i, list) =>
@@ -127,11 +156,13 @@ function Data({ version = "v1" }) {
         }))
         .filter(
           (item: any) =>
+            (!filters.category ||
+              (item.Categories || []).includes(filters.category)) &&
             (item._filter.match(filter) || filter.trim() === String(item.id)) &&
-            priceFrom <= item._price &&
-            item._price <= priceTo
+            filters.priceFrom <= item._price &&
+            item._price <= filters.priceTo
         ),
-    [results, filter, priceFrom, priceTo]
+    [results, filter, filters.category, filters.priceFrom, filters.priceTo]
   );
 
   const sorted = useMemo(
@@ -146,8 +177,39 @@ function Data({ version = "v1" }) {
     <div>
       <fieldset>
         <label>
+          <span>Category</span>
+          <select
+            value={filters.category}
+            onChange={useCallback(
+              ({ target }) =>
+                setFilters((filters) => ({
+                  ...filters,
+                  category: target.value,
+                })),
+              []
+            )}
+          >
+            {options.category.map((value) => (
+              <option key={value} value={value}>
+                {value}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label>
           <span>Search</span>
-          <input type="search" value={search} onChange={onChangeSearch} />
+          <input
+            type="search"
+            value={filters.search}
+            onChange={useCallback(
+              ({ target }) =>
+                setFilters((filters) => ({
+                  ...filters,
+                  search: target.value,
+                })),
+              []
+            )}
+          />
         </label>
         <label>
           <span>Sort</span>
@@ -167,7 +229,7 @@ function Data({ version = "v1" }) {
               list="price-list"
               min={PRICE_LIST[0]}
               max={PRICE_LIST[PRICE_LIST.length - 1]}
-              value={priceFrom}
+              value={filters.priceFrom}
               onChange={onChangePriceFrom}
             />
             <datalist id="price-list">
@@ -189,10 +251,10 @@ function Data({ version = "v1" }) {
               list="price-list"
               min={PRICE_LIST[0]}
               max={PRICE_LIST[PRICE_LIST.length - 1]}
-              value={priceTo}
+              value={filters.priceTo}
               onChange={onChangePriceTo}
             />
-            <span>{`${priceFrom}-${priceTo} pln`}</span>
+            <span>{`${filters.priceFrom}-${filters.priceTo} pln`}</span>
           </label>
         </div>
       </fieldset>
@@ -205,17 +267,26 @@ function Data({ version = "v1" }) {
               _history,
               ...rest
             }: {
+              Categories: [string];
               Images: string[];
               LastModifiedDate: Date;
+              OriginalReleaseDate: Date;
               Price: any;
               ProductId: string;
               ProductTitle: string;
               PublisherName: string;
+              UsageData: [
+                {
+                  AggregateTimeSpan: string;
+                  AverageRating: number;
+                  RatingCount: number;
+                }
+              ];
               _history: any;
             },
             key: number
           ) => (
-            <li key={key} className={styles.Row}>
+            <li key={rest.ProductId} className={styles.Row}>
               <Gallery className={styles.Gallery} images={Images} />
               <Summary {...rest} />
               <History history={_history} />
@@ -257,7 +328,6 @@ function Summary({
   ProductId,
   ProductTitle,
   PublisherName,
-  UsageData,
 }: {
   Categories: [string];
   OriginalReleaseDate: Date;
@@ -265,16 +335,15 @@ function Summary({
   ProductId: string;
   ProductTitle: string;
   PublisherName: string;
-  UsageData: [
-    {
-      AggregateTimeSpan: string;
-      AverageRating: number;
-      RatingCount: number;
-    }
-  ];
 }) {
   return (
     <div className={styles.Summary}>
+      {OriginalReleaseDate && (
+        <h6>
+          <span>OriginalReleaseDate: </span>
+          {format(OriginalReleaseDate, "yyyy-MM-dd")}
+        </h6>
+      )}
       <h3>
         <Link href={createHref({ ProductId, ProductTitle })}>
           {ProductTitle}
@@ -282,28 +351,10 @@ function Summary({
       </h3>
       <h4>{PublisherName}</h4>
       {Categories && (
-        <div>
+        <h5>
           <span>Categories: </span>
           {Categories.join(", ")}
-        </div>
-      )}
-      {OriginalReleaseDate && (
-        <div>
-          <span>OriginalReleaseDate: </span>
-          {format(OriginalReleaseDate, "yyyy-MM-dd HH:mm")}
-        </div>
-      )}
-      {UsageData && (
-        <div>
-          <span>AverageRating: </span>
-          {UsageData.map(
-            ({ AggregateTimeSpan, AverageRating, RatingCount }, key) => (
-              <span key={key}>
-                {AggregateTimeSpan}: {AverageRating} ({RatingCount}){" "}
-              </span>
-            )
-          )}
-        </div>
+        </h5>
       )}
       {/* <div>{format(LastModifiedDate, "yyyy-MM-dd HH:mm")}</div> */}
     </div>
@@ -313,6 +364,7 @@ function Summary({
 function Details({
   LastModifiedDate,
   Price,
+  UsageData,
 }: {
   LastModifiedDate: Date;
   Price: {
@@ -322,6 +374,13 @@ function Details({
     WholesaleCurrencyCode: string;
     WholesalePrice: number;
   };
+  UsageData: [
+    {
+      AggregateTimeSpan: string;
+      AverageRating: number;
+      RatingCount: number;
+    }
+  ];
 }) {
   return (
     <div className={styles.Details}>
@@ -340,7 +399,22 @@ function Details({
           {[Price.WholesalePrice, Price.WholesaleCurrencyCode].join(" ")}
         </span>
       </h5>
-      <div>{format(LastModifiedDate, "yyyy-MM-dd HH:mm")}</div>
+      <div>
+        <span>{format(LastModifiedDate, "yyyy-MM-dd HH:mm")}</span>
+        {UsageData && (
+          <span>
+            {" -- "}
+            <span>AverageRating: </span>
+            {UsageData.map(
+              ({ AggregateTimeSpan, AverageRating, RatingCount }, key) => (
+                <span key={key}>
+                  {AverageRating} ({RatingCount}/{AggregateTimeSpan}){" "}
+                </span>
+              )
+            )}
+          </span>
+        )}
+      </div>
     </div>
   );
 }
