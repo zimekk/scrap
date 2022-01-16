1c1a0dbe23afb0d04bc542f6de3bc042e2d30424
Marcin Makarewicz <marcin@makarewicz.eu>
2022-01-16 15:58:53 +0100

    feat(web): handle gallery images load error

--- packages/web/src/components/Gallery/index.tsx
+++ packages/web/src/components/Gallery/index.tsx
@@ -1,23 +1,30 @@
 import React, { Suspense, useEffect, useRef, useState } from "react";
 import { createAsset } from "use-asset";
+import { faImage } from "@fortawesome/free-solid-svg-icons";
 import { Spinner } from "../Spinner";
 import cx from "classnames";
 import styles from "./styles.module.scss";
 
-const image = createAsset(async (src) => {
-  return new Promise((onload) => {
-    const img = new Image();
-    Object.assign(img, {
-      onload,
-      src,
-    });
-  });
-});
+const imgSrcFallback = () =>
+  (({ icon: [w, h, , , d] }) =>
+    `data:image/svg+xml;utf8,<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="crosshairs" class="svg-inline--fa fa-crosshairs fa-w-16 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${w} ${h}"><path fill="currentColor" d="${d}"></path></svg>`)(
+    faImage
+  );
+
+const image = createAsset(
+  async (src): Promise<string> =>
+    new Promise((resolve) => {
+      const img = new Image();
+      Object.assign(img, {
+        onload: () => resolve(src),
+        onerror: () => resolve(imgSrcFallback()),
+        src,
+      });
+    })
+);
 
 function Img({ src, ...props }: { src: string }) {
-  image.read(src);
-
-  return <img src={src} {...props} referrerPolicy="no-referrer" />;
+  return <img src={image.read(src)} {...props} referrerPolicy="no-referrer" />;
 }
 
 function Loader() {
