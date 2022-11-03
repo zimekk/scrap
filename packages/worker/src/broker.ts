2665bf9f0683c3fff74bd523832d80537965d074
Marcin Makarewicz <marcin@makarewicz.eu>
2022-11-03 23:10:40 +0100

    refactor(worker): separate broker and worker

--- packages/worker/src/broker.ts
+++ packages/worker/src/broker.ts
@@ -1,1 +1,100 @@
-export { broker } from "./worker";
+import { minutes } from "milliseconds";
+import { Names } from "./constants";
+import { queue } from "./worker";
+
+const JOB_ID = "broker";
+
+export const broker = async () => {
+  console.log(["broker"]);
+
+  // await queue
+  //   .add({
+  //     type: "get-hot-shot:goracy-strzal",
+  //   })
+  //   .then(({ id, data }) => console.log(["add"], { id, data }));
+
+  // await queue
+  //   .add({
+  //     type: "get-hot-shot-alto:goracy-strzal",
+  //   })
+  //   .then(({ id, data }) => console.log(["add"], { id, data }));
+
+  await Promise.all(
+    (
+      await queue.getRepeatableJobs()
+    ).map(
+      async ({ key }) =>
+        Boolean(console.log(["removeRepeatableByKey"], { key })) ||
+        (await queue.removeRepeatableByKey(key))
+    )
+  );
+
+  // await queue
+  //   .add(
+  //     {
+  //       type: "otodom:sprzedaz/dom/cala-polska",
+  //     },
+  //     {
+  //       jobId: JOB_ID,
+  //       repeat: { every: minutes(1) },
+  //     }
+  //   )
+  //   .then(({ id, data }) => console.log(["add"], { id, data }));
+
+  await queue
+    .add(
+      Names.HOTSHOT,
+      {
+        type: "get-hot-shot:goracy-strzal",
+      },
+      {
+        repeat: { cron: "1 10,22 * * *" },
+      }
+    )
+    .then(({ id, data }) => console.log(["add"], { id, data }));
+
+  await queue
+    .add(
+      Names.HOTSHOT,
+      {
+        type: "get-hot-shot-alto:goracy-strzal",
+      },
+      {
+        repeat: { cron: "1 9,21 * * *" },
+      }
+    )
+    .then(({ id, data }) => console.log(["add"], { id, data }));
+
+  await queue
+    .add(
+      Names.OTODOM,
+      {
+        type: "otodom:sprzedaz/dom/cala-polska",
+      },
+      {
+        repeat: { every: minutes(15) },
+      }
+    )
+    .then(({ id, data }) => console.log(["add"], { id, data }));
+
+  await queue
+    .addBulk(
+      [
+        "otodom:sprzedaz/dom/komorow_5600",
+        "otodom:sprzedaz/dom/michalowice_62659",
+        "otodom:sprzedaz/dom/stare-babice",
+      ].map((type) => ({
+        name: Names.PROPERTY_OTODOM,
+        data: { type },
+        opts: {},
+      }))
+    )
+    .then((jobs) =>
+      console.log(
+        ["add"],
+        jobs.map(({ id }) => id)
+      )
+    );
+
+  await queue.close().then(() => console.log(["close"]));
+};
--- packages/worker/src/worker.ts
+++ packages/worker/src/worker.ts
@@ -1,12 +1,11 @@
 import Queue from "bull";
-import { minutes } from "milliseconds";
 import { Names } from "./constants";
 
 type Summary = {
   request: Record<string, { number?: number; length?: number }>;
 };
 
-const queue = new Queue("scrap", {
+export const queue = new Queue("scrap", {
   limiter: {
     max: 1, // Max number of jobs processed
     duration: 5000, // per duration in milliseconds
@@ -18,103 +17,6 @@ const queue = new Queue("scrap", {
   .on("completed", ({ id, name, data, opts, finishedOn }, result) =>
     console.log(["completed"], result, { id, name, data, opts, finishedOn })
   );
-
-const JOB_ID = "broker";
-
-export const broker = async () => {
-  console.log(["broker"]);
-
-  // await queue
-  //   .add({
-  //     type: "get-hot-shot:goracy-strzal",
-  //   })
-  //   .then(({ id, data }) => console.log(["add"], { id, data }));
-
-  // await queue
-  //   .add({
-  //     type: "get-hot-shot-alto:goracy-strzal",
-  //   })
-  //   .then(({ id, data }) => console.log(["add"], { id, data }));
-
-  await Promise.all(
-    (
-      await queue.getRepeatableJobs()
-    ).map(
-      async ({ key }) =>
-        Boolean(console.log(["removeRepeatableByKey"], { key })) ||
-        (await queue.removeRepeatableByKey(key))
-    )
-  );
-
-  // await queue
-  //   .add(
-  //     {
-  //       type: "otodom:sprzedaz/dom/cala-polska",
-  //     },
-  //     {
-  //       jobId: JOB_ID,
-  //       repeat: { every: minutes(1) },
-  //     }
-  //   )
-  //   .then(({ id, data }) => console.log(["add"], { id, data }));
-
-  await queue
-    .add(
-      Names.HOTSHOT,
-      {
-        type: "get-hot-shot:goracy-strzal",
-      },
-      {
-        repeat: { cron: "1 10,22 * * *" },
-      }
-    )
-    .then(({ id, data }) => console.log(["add"], { id, data }));
-
-  await queue
-    .add(
-      Names.HOTSHOT,
-      {
-        type: "get-hot-shot-alto:goracy-strzal",
-      },
-      {
-        repeat: { cron: "1 9,21 * * *" },
-      }
-    )
-    .then(({ id, data }) => console.log(["add"], { id, data }));
-
-  await queue
-    .add(
-      Names.OTODOM,
-      {
-        type: "otodom:sprzedaz/dom/cala-polska",
-      },
-      {
-        repeat: { every: minutes(15) },
-      }
-    )
-    .then(({ id, data }) => console.log(["add"], { id, data }));
-
-  await queue
-    .addBulk(
-      [
-        "otodom:sprzedaz/dom/komorow_5600",
-        "otodom:sprzedaz/dom/michalowice_62659",
-        "otodom:sprzedaz/dom/stare-babice",
-      ].map((type) => ({
-        name: Names.PROPERTY_OTODOM,
-        data: { type },
-        opts: {},
-      }))
-    )
-    .then((jobs) =>
-      console.log(
-        ["add"],
-        jobs.map(({ id }) => id)
-      )
-    );
-
-  await queue.close().then(() => console.log(["close"]));
-};
 
 const summary = {
   created: [],
