1ea61fa69ca76257dcb93f09cb7aa3851b4c7c9e
Marcin Makarewicz <marcin@makarewicz.eu>
2023-01-10 23:53:20 +0100

    chore(cli): extend moto schema

--- packages/cli/src/services/MotoService/types.ts
+++ packages/cli/src/services/MotoService/types.ts
@@ -2,7 +2,30 @@ import { z } from "zod";
 
 export const MotoItem = z.object({
   id: z.string(),
+  location: z
+    .object({
+      city: z.object({
+        name: z.string(),
+        // __typename: z.enum(["AdministrativeLevel"]),
+      }),
+      region: z.object({
+        name: z.string(),
+        // __typename: z.enum(["AdministrativeLevel"]),
+      }),
+      // __typename: z.enum(["Location"]),
+    })
+    .optional(),
+  parameters: z
+    .object({
+      key: z.enum(["make", "year", "mileage", "engine_capacity", "fuel_type"]),
+      displayValue: z.string(),
+      value: z.string(),
+      // __typename: z.enum(["AdvertParameter"]),
+    })
+    .array()
+    .optional(),
   price: z.number(),
+  shortDescription: z.string().optional(),
   thumbnail: z.string().optional(),
   title: z.string(),
   url: z.string(),
