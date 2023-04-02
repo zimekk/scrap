90e6e2915f569824aad446824368d8c2d4173723
Marcin Makarewicz <marcin@makarewicz.eu>
2023-04-02 20:46:22 +0200

    feat(cli, web): gpass history

--- packages/cli/src/services/GpassService/types.ts
+++ packages/cli/src/services/GpassService/types.ts
@@ -11,10 +11,28 @@ export const DiffSchema = z.object({
   details: z.object({}).passthrough().array(),
 });
 
+export interface DetailsType {
+  link: {
+    href: string;
+    text: string;
+  };
+  intro: string;
+  price: string;
+  priceNumber: string;
+  bullets: string[];
+}
+
 export type GpassType = z.infer<typeof Gpass> & {
+  details: DetailsType[];
   _id: string;
   _created: number;
   _checked: number;
   _updated: number;
-  _history: Record<string, object>;
+  _history: Record<
+    string,
+    {
+      details: DetailsType[];
+      _checked: number;
+    }
+  >;
 };
--- packages/web/src/containers/Games/Gpass/index.tsx
+++ packages/web/src/containers/Games/Gpass/index.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, {
+  type MouseEventHandler,
+  useCallback,
+  useMemo,
+  useState,
+} from "react";
+import { format } from "date-fns";
 import { createAsset } from "use-asset";
 import { Json } from "../../../components/Json";
+import { Link } from "../../../components/Link";
+import styles from "./styles.module.scss";
 
-import type { GpassType } from "@dev/cli/src/services/GpassService/types";
+import type {
+  DetailsType,
+  GpassType,
+} from "@dev/cli/src/services/GpassService/types";
+
+type ItemType = {
+  updated: number;
+  details: DetailsType[];
+};
 
 // https://github.com/pmndrs/use-asset#dealing-with-async-assets
 const asset = createAsset(async (version) => {
@@ -10,12 +26,98 @@ const asset = createAsset(async (version) => {
   return await res.json().then<GpassType[]>((result) => result);
 });
 
+function Details({
+  item,
+  expanded,
+  onExpand,
+}: {
+  item: ItemType;
+  expanded: boolean;
+  onExpand: () => void;
+}) {
+  const handleExpand = useCallback<MouseEventHandler<HTMLAnchorElement>>(
+    (e) => (e.preventDefault(), onExpand()),
+    []
+  );
+
+  return (
+    <div className={styles.Details}>
+      <div className={styles.Divider}>
+        <Link onClick={handleExpand}>
+          {format(item.updated, "yyyy-MM-dd HH:mm")}
+        </Link>
+      </div>
+      {expanded && (
+        <table>
+          <tbody>
+            <tr>
+              {item.details.map(
+                ({ intro, link, bullets, price, priceNumber }, key) => (
+                  <td key={key}>
+                    <h4>{intro}</h4>
+                    <div>{priceNumber}</div>
+                    <Link href={link.href}>{link.text}</Link>
+                    <div>{price}</div>
+                    <ul>
+                      {bullets.map((text: string, key: number) => (
+                        <li key={key}>{text}</li>
+                      ))}
+                    </ul>
+                  </td>
+                )
+              )}
+            </tr>
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+}
+
+function History({ history }: { history: ItemType[] }) {
+  const [expanded, onExpand] = useState([0]);
+
+  return (
+    <div className={styles.History}>
+      {history.map((item, key) => (
+        <Details
+          key={key}
+          item={item}
+          expanded={expanded.includes(key)}
+          onExpand={() =>
+            onExpand((expanded) =>
+              expanded.includes(key)
+                ? expanded.filter((index) => index !== key)
+                : expanded.concat(key)
+            )
+          }
+        />
+      ))}
+    </div>
+  );
+}
+
 function Data({ version = "v1" }) {
   const results = asset.read(version); // As many cache keys as you need
 
-  console.log({ results });
+  const history = useMemo(
+    () =>
+      (([{ _updated, _history, ...item }]) =>
+        Object.entries(_history)
+          .map(([_updated, item]) => ({ updated: Number(_updated), ...item }))
+          .concat({ updated: _updated, ...item })
+          .sort((a, b) => b.updated - a.updated))(results),
+    [results]
+  );
 
-  return <Json>{results}</Json>;
+  console.log({ results, history });
+
+  return (
+    <section className={styles.Section}>
+      <History history={history} />
+      <Json>{results}</Json>
+    </section>
+  );
 }
 
 export default function Section() {
