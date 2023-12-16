a3268dc10b27a3e5c2cb9400480d68bfad6484d4
Marcin Makarewicz <marcin@makarewicz.eu>
2023-12-16 09:53:34 +0100

    feat(web): mobile balance

--- packages/web/src/containers/Mobile/data.ts
+++ packages/web/src/containers/Mobile/data.ts
@@ -152,6 +152,47 @@ export const operations = [
 
 export const balance = [
   {
+    date: "2023-12-15",
+    number: "183",
+    amount: 13.95,
+    net: 1_170,
+    min: "70:05",
+    sms: 90,
+  },
+  {
+    date: "2023-12-15",
+    number: "938",
+    amount: 5.94,
+    net: 976,
+    min: 165,
+    sms: 208,
+  },
+  {
+    date: "2023-12-15",
+    number: "379",
+    amount: 2.72,
+    net: 8_560,
+    min: "1:22",
+    sms: 130,
+  },
+  {
+    date: "2023-12-15",
+    number: "818",
+    amount: 16.11,
+    net: 3_240,
+    min: 166,
+    sms: 169,
+  },
+  {
+    date: "2023-12-15",
+    number: "828",
+    amount: 5.8,
+    net: 1_610,
+    min: 102,
+    sms: 180,
+  },
+
+  {
     date: "2023-12-10",
     number: "183",
     amount: 13.95,
