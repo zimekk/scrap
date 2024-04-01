8fee9f40110c281c468eac4df1f0b03c4564fb20
Marcin Makarewicz <marcin@makarewicz.eu>
2024-04-01 23:05:39 +0200

    fix(web): mobile balance

--- packages/web/src/containers/Mobile/data.ts
+++ packages/web/src/containers/Mobile/data.ts
@@ -1,4 +1,10 @@
 export const operations = [
+  { date: "2024-04-01", number: "183", amount: 10 },
+  { date: "2024-04-01", number: "938", amount: 5 },
+  { date: "2024-04-01", number: "379", amount: 25 },
+  { date: "2024-04-01", number: "818", amount: 5 },
+  { date: "2024-04-01", number: "828", amount: 10 },
+
   { date: "2024-03-30", number: "828", amount: -5, net: "1GB" },
 
   { date: "2024-03-10", number: "183", amount: -9, net: "2GB" },
@@ -182,6 +188,52 @@ export const operations = [
 ];
 
 export const balance = [
+  {
+    date: "2024-04-01",
+    number: "183",
+    // amount: 4.95,
+    amount: 14.95,
+    net: 962,
+    min: 121,
+    sms: 54,
+  },
+  {
+    date: "2024-04-01",
+    number: "938",
+    // amount: 10.35,
+    amount: 15.35,
+    net: 1_600,
+    min: 156,
+    sms: 162,
+  },
+  {
+    date: "2024-04-01",
+    number: "379",
+    // amount: 2.72,
+    amount: 27.72,
+    net: 3_810,
+    min: "1:22",
+    sms: 130,
+  },
+  {
+    date: "2024-04-01",
+    number: "818",
+    // amount: 11.11,
+    amount: 16.11,
+    net: 4_320,
+    min: 166,
+    sms: 167,
+  },
+  {
+    date: "2024-04-01",
+    number: "828",
+    // amount: 0.21,
+    amount: 10.21,
+    net: 506, // ?
+    min: 124,
+    sms: 167,
+  },
+
   {
     date: "2024-03-30",
     number: "183",
