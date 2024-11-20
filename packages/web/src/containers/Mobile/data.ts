9db3ee743105d97a3000ae0f95dcdf11041ba930
Marcin Makarewicz <marcin@makarewicz.eu>
2024-11-20 19:19:34 +0100

    fix(web): mobile balance

--- packages/web/src/containers/Mobile/data.ts
+++ packages/web/src/containers/Mobile/data.ts
@@ -268,6 +268,47 @@ export const operations = [
 
 export const balance = [
   {
+    date: "2024-11-20",
+    number: "183",
+    amount: 3.95,
+    net: 2_570,
+    min: 146,
+    sms: 115,
+  },
+  {
+    date: "2024-11-20",
+    number: "938",
+    amount: 8.19,
+    net: 2_720,
+    min: 209,
+    sms: 143,
+  },
+  {
+    date: "2024-11-20",
+    number: "379",
+    amount: 5.34,
+    net: 7_830,
+    min: "1:22",
+    sms: 130,
+  },
+  {
+    date: "2024-11-20",
+    number: "818",
+    amount: 11.11,
+    net: 3_740,
+    min: 265,
+    sms: 162,
+  },
+  {
+    date: "2024-11-20",
+    number: "828",
+    amount: 5.43,
+    net: 990,
+    min: "84:36",
+    sms: 166,
+  },
+
+  {
     date: "2024-11-15",
     number: "183",
     // amount: 12.95,
