b3d370993f39f621d543cec3a63c25759706e6a5
Marcin Makarewicz <marcin@makarewicz.eu>
2025-01-15 23:56:28 +0100

    fix: add puppeteer config

---
+++ puppeteer.config.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+  // Download Chrome (default `skipDownload: false`).
+  chrome: {
+    skipDownload: true,
+  },
+  // Download Firefox (default `skipDownload: true`).
+  firefox: {
+    skipDownload: true,
+  },
+};
