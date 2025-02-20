f4eed67b1cbb61014a6148ba3e6ea641e8345975
Marcin Makarewicz <marcin@makarewicz.eu>
2025-02-20 16:54:38 +0100

    fix(web): exchange and mobile balance

--- packages/web/src/containers/Mobile/data.ts
+++ packages/web/src/containers/Mobile/data.ts
@@ -297,11 +297,62 @@ export const operations = [
 
 export const balance = [
   {
+    date: "2025-02-20",
+    number: "183",
+    amount: 8.95,
+    net: 4_490, //
+    roaming: 2_790,
+    min: 109,
+    sms: 177, //
+  },
+  {
+    date: "2025-02-20",
+    number: "938",
+    amount: 2.4,
+    net: 1_900,
+    min: 261, //
+    sms: 118,
+  },
+  {
+    date: "2025-02-20",
+    number: "379",
+    amount: 0.34, //
+    net: 8_420,
+    roaming: 6_390,
+    min: "1:22",
+    sms: 130,
+  },
+  {
+    date: "2025-02-20",
+    number: "818",
+    amount: 12.11, //
+    net: 4_970,
+    roaming: 2_530,
+    min: 265,
+    sms: 155,
+  },
+  {
+    date: "2025-02-20",
+    number: "828",
+    amount: 1.06,
+    net: 1_270,
+    roaming: 7_610,
+    min: "46:25", // ?
+    sms: 95,
+  },
+  {
+    date: "2025-02-20",
+    number: "102",
+    net: 149_310,
+    roaming: 8_450,
+  },
+
+  {
     date: "2025-02-15",
     number: "183",
     amount: 8.95,
-    net: 2_820, //
-    roaming: 5_17,
+    net: 4_820, //
+    roaming: 5_170,
     min: 110,
     sms: 177, //
   },
@@ -352,7 +403,7 @@ export const balance = [
     number: "183",
     amount: 8.95,
     net: 5_130, //
-    roaming: 5_17,
+    roaming: 5_170,
     min: 119,
     sms: 178, //
   },
@@ -403,7 +454,7 @@ export const balance = [
     number: "183",
     amount: 8.95,
     net: 5_300, //
-    roaming: 5_34,
+    roaming: 5_340,
     min: 122,
     sms: 178, //
   },
