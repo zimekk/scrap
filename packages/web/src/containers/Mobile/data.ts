a708751aabf2ac13c7b7eb3cfa2b18720eb90eb8
zimekk <zimekk@gmail.com>
2026-04-26 00:55:46 +0200

    fix(web): mobile balance

--- packages/web/src/containers/Mobile/data.ts
+++ packages/web/src/containers/Mobile/data.ts
@@ -389,6 +389,142 @@ export const operations = [
 
 export const balance = [
   {
+    date: "2026-04-30",
+    number: "183",
+    amount: 0.95, // ważne do 18 maja 2027
+    net: 2.11 * GB,
+    roaming: 6.18 * GB,
+    min: 103, //
+    sms: 66,
+    operations: [], //
+  },
+  {
+    date: "2026-04-30",
+    number: "938",
+    amount: 6.08, // ważne do 14 kwietnia 2027
+    net: 1.25 * GB,
+    roaming: 0 * GB,
+    min: 149,
+    sms: 109,
+    operations: [], //
+  },
+  {
+    date: "2026-04-30",
+    number: "379",
+    amount: 1.37, // ważne do 24 kwietnia 2027
+    net: 5.01 * GB,
+    roaming: 8.59 * GB,
+    min: "1:22",
+    sms: 130,
+    operations: [], //
+  },
+  {
+    date: "2026-04-30",
+    number: "818",
+    amount: 0.97, // ważne do 30 kwietnia 2027
+    net: 1.21 * GB,
+    roaming: 6.53 * GB,
+    min: 255,
+    sms: 141,
+    operations: [], //
+  },
+  {
+    date: "2026-04-30",
+    number: "828",
+    amount: 0.01, // ważne do 30 kwietnia 2027
+    net: 945 * MB,
+    roaming: 6.18 * GB,
+    min: "00:00",
+    sms: 66,
+    operations: [], //
+  },
+  {
+    date: "2026-04-30",
+    number: "796",
+    // amount: 60, // konto utraciło waność / traci ważność dziś / traci ważność za 5 dni
+    amount: 65, // konto utraciło waność
+    net: 0 * GB,
+    roaming: 0 * GB,
+    min: 10,
+    sms: 100,
+    operations: [], //
+  },
+  {
+    date: "2026-04-30",
+    number: "102",
+    roaming: 10.3 * GB,
+    net: 148.56 * GB,
+  },
+
+  {
+    date: "2026-04-25",
+    number: "183",
+    amount: 0.95, // ważne do 18 maja 2027
+    net: 2.22 * GB,
+    roaming: 6.18 * GB,
+    min: 103, //
+    sms: 69,
+    operations: [], //
+  },
+  {
+    date: "2026-04-25",
+    number: "938",
+    amount: 6.08, // ważne do 14 kwietnia 2027
+    net: 1.28 * GB,
+    roaming: 0 * GB,
+    min: 150,
+    sms: 109,
+    operations: [], //
+  },
+  {
+    date: "2026-04-25",
+    number: "379",
+    amount: 1.37, // ważne do 24 kwietnia 2027
+    net: 6.11 * GB,
+    roaming: 8.59 * GB,
+    min: "1:22",
+    sms: 130,
+    operations: [], //
+  },
+  {
+    date: "2026-04-25",
+    number: "818",
+    amount: 0.97, // ważne do 30 kwietnia 2027
+    net: 1.27 * GB,
+    roaming: 6.53 * GB,
+    min: 255,
+    sms: 141,
+    operations: [], //
+  },
+  {
+    date: "2026-04-25",
+    number: "828",
+    amount: 0.01, // ważne do 30 kwietnia 2027
+    net: 1.18 * GB,
+    roaming: 6.18 * GB,
+    min: "00:00",
+    sms: 71,
+    operations: [], //
+  },
+  {
+    date: "2026-04-25",
+    number: "796",
+    // amount: 60, // konto utraciło waność / traci ważność dziś / traci ważność za 5 dni
+    amount: 65, // konto utraciło waność
+    net: 0 * GB,
+    roaming: 0 * GB,
+    min: 10,
+    sms: 100,
+    operations: [], //
+  },
+  {
+    date: "2026-04-25",
+    number: "102",
+    roaming: 10.3 * GB,
+    net: 148.56 * GB,
+  },
+
+  {
     date: "2026-04-20",
     number: "183",
     // amount: 4.95, // ważne do 18 kwietnia 2027
